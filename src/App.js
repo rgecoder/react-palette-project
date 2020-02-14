3bfd9742442d0745f8ed891b973a48674c742d85
Roy Ge <rgecoder@gmail.com>
2020-02-13 16:36:56 -0800

    Add ColorBox component

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import seedColors from './seedColors';
 function App() {
   return (
     <div className="App">
-      <Palette {...seedColors[5]}/>
+      <Palette {...seedColors[6]}/>
     </div>
   );
 }

4d0947d94b421036a817c0299a3b2c6ad59006e0
Roy Ge <rgecoder@gmail.com>
2020-02-19 21:24:24 -0800

    Add colorHelpers.js

--- src/colorHelpers.js
+++ src/colorHelpers.js
@@ -25,34 +25,48 @@
 //   ]
 // }
 
-import chroma from 'chroma-js';
-const levels = [50,100,200,400];
+import chroma from "chroma-js";
+const levels = [50, 100, 200, 400];
 
-function generatePalette(starterPalette){
-  let newPalette ={
+function generatePalette(starterPalette) {
+  let newPalette = {
     paletteName: starterPalette.paletteName,
     id: starterPalette.id,
     emoji: starterPalette.emoji,
-    colors: {
-      
-    }
-  }
-  for (let level of levels){
+    colors: {}
+  };
+  for (let level of levels) {
     newPalette.colors[level] = [];
   }
-  for (let color of starterPalette.colors){
-
+  for (let color of starterPalette.colors) {
+    let scale = getScale(color.color, 10).reverse();
+    for (let i in scale) {
+      newPalette.colors[levels[i]].push({
+        name: `{color.name} ${levels[i]}`,
+        id: color.name.toLowerCase().replace(/ /g, "-"),
+        hex: scale[i],
+        rgb: chroma(scale[i]).css()
+      });
+    }
   }
+  return newPalette;
 }
-function getRange(hexColor){
-  const end="#fff";
+function getRange(hexColor) {
+  const end = "#fff";
   return [
-    chroma(hexColor).darken(1.4).hex(),
+    chroma(hexColor)
+      .darken(1.4)
+      .hex(),
     hexColor,
     end
-  ]
+  ];
 }
 
-function generateScale(hexColor, numberOfColors){
-  chroma.scale()
+function getScale(hexColor, numberOfColors) {
+  return chroma
+    .scale(getRange(hexColor))
+    .mode("lab")
+    .colors(numberOfColors);
 }
+
+export {generatePalette};

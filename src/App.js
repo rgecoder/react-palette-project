b2208cadf3eeee961dc4d989b761c0668916673e
Roy Ge <rgecoder@gmail.com>
2020-02-21 15:35:29 -0800

    Add slider v1.0

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import {generatePalette} from "./colorHelpers";
 function App() {
   return (
     <div className="App">
-      <Palette {...seedColors[6]}/>
+      <Palette palette={generatePalette(seedColors[4])}/>
     </div>
   );
 }

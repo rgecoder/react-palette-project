eab42e3eade60420bca38922f9ed1593e037ec60
Roy Ge <rgecoder@gmail.com>
2020-02-23 16:34:38 -0800

    Add SingleColorPalette and link with moreUrl prop

--- src/Palette.js
+++ src/Palette.js
@@ -13,21 +13,33 @@ class Palette extends Component {
   changeLevel(level) {
     this.setState({ level });
   }
-  changeFormat(val){
-    this.setState({format: val});
+  changeFormat(val) {
+    this.setState({ format: val });
   }
   render() {
-    const { colors, paletteName, emoji } = this.props.palette;
+    const { colors, paletteName, emoji, id } = this.props.palette;
     const { level, format } = this.state;
     const colorBoxes = colors[level].map(color => (
-      <ColorBox background={color[format]} name={color.name} key={color.id} />
+      <ColorBox
+        background={color[format]}
+        name={color.name}
+        key={color.id}
+        id={color.id}
+        paletteId={id}
+        moreUrl={`/palette/${id}/${color.id}`}
+        
+      />
     ));
     return (
       <div className="Palette">
-        <Navbar level={level} changeLevel={this.changeLevel} handleChange={this.changeFormat}/>
+        <Navbar
+          level={level}
+          changeLevel={this.changeLevel}
+          handleChange={this.changeFormat}
+        />
 
         <div className="Palette-colors">{colorBoxes}</div>
-        <footer className='Palette-footer'>
+        <footer className="Palette-footer">
           {paletteName}
           <span className="emoji">{emoji}</span>
         </footer>
